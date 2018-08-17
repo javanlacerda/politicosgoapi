5b063986717377066eaf536e2d6bcb341908f1fd
javanlacerda <javan.lacerda@ccc.ufcg.edu.br>
2018-08-17 11:51:54 -0300

    add project's base

---
+++ politicosgo/src/main/java/com/example/politicosgo/PoliticosgoApplication.java
@@ -0,0 +1,12 @@
+package com.example.politicosgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliticosgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoliticosgoApplication.class, args);
+	}
+}
